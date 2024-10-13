20edcf851d260f34a8b9cd51bf73976a7ddeee9a
Abdullah Al Arif <faithcoder1@gmail.com>
2024-10-14 00:02:31 +0600

    custom block added

--- functions.php
+++ functions.php
@@ -62,3 +62,36 @@ add_action( 'wp_enqueue_scripts', 'shadhin_enqueue_custom_styles' );
 //     wp_add_inline_style( 'shadhin-style', $custom_css );
 // }
 // add_action( 'wp_enqueue_scripts', 'shadhin_enqueue_styles' );
+
+
+function shadhin_register_block_patterns() {
+    if ( function_exists( 'register_block_pattern' ) ) {
+        register_block_pattern(
+            'shadhin/custom-pattern',
+            array(
+                'title'       => __( 'Shadhin Custom Pattern', 'shadhin' ),
+                'description' => _x( 'A custom block pattern for the Shadhin theme.', 'Block pattern description', 'shadhin' ),
+                'content'     => "<!-- wp:paragraph --><p>" . __( 'This is a custom block pattern from the Shadhin theme.', 'shadhin' ) . "</p><!-- /wp:paragraph -->",
+            )
+        );
+    }
+}
+add_action( 'init', 'shadhin_register_block_patterns' );
+
+
+function shadhin_register_block_styles() {
+    if ( function_exists( 'register_block_style' ) ) {
+        // Register a custom style for core paragraph block
+        register_block_style(
+            'core/paragraph',
+            array(
+                'name'  => 'shadhin-custom-paragraph',
+                'label' => __( 'Shadhin Custom Paragraph', 'shadhin' ),
+            )
+        );
+        // You can add more block styles here for other core blocks
+    }
+}
+add_action( 'init', 'shadhin_register_block_styles' );
+
+
